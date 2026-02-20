0072201638d1a015935f73ec1354c0d761581e82
redeemflores488-sudo <redeemflores488@gmail.com>
2026-02-20 16:41:56 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Serve static files from public folder
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   // RAILWAY PORT CONFIGURATION
   const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0'); // '0.0.0.0' is required for Railway!
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
-
