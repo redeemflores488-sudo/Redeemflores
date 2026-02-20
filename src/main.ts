6bac63d1e6359bcecbb4bb2dac028525f89e7018
redeemflores488-sudo <redeemflores488@gmail.com>
2026-02-20 08:47:05 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // <--- ADD THIS LINE
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
-
-  console.log('Application is running on: ${await app.getUrl()}');
+  
+  // RAILWAY PORT CONFIGURATION
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0'); // '0.0.0.0' is required for Railway!
 }
 bootstrap();
+
